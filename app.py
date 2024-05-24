0ec40083c1f5c0565277538f63f62f1291cf7cb2
James Coker <1jamescoker1@gmail.com>
2024-05-24 17:36:40 +0100

    Add barchart

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, dcc
 import pandas as pd
+import plotly.express as px
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
@@ -7,7 +8,8 @@ app = Dash()
 
 app.layout = [
     html.Div(children='My first App with Data'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
 ]
 
 if __name__ == '__main__':
